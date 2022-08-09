f61e64b4d7c53ac34a60195e114acf53a0975426
Olivia <oliviatmanalastas@gmail.com>
2022-08-08 20:30:27 -0400

    commit tree e32a8dca0ecbc8da4884ea2df95b89809d9b6ce9

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+ def greet_programmer
+   puts "Hello, programmer!"
+ end
+
+ def greet (param)
+  puts "Hello, #{param}!"
+ end
+
+ def greet_with_default (param = 'programmer')
+  puts "Hello, #{param}!"
+ end
+
+ def add (value1, value2)
+  value1 + value2
+ end
+
+ def halve (num)
+  if num.class != Integer
+    return nil
+  end
+  num / 2
+ end
